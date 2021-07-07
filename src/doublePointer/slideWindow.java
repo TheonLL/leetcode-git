dcf172f2bc8437fc57d930ed36e1bcdf4363d44d
glence <GLruan@foxmail.com>
2021-07-07 11:41:24 +0800

    滑动窗口类型，增加了最长子不重复字符串，弄明白了匹配字符串中有重复字符的问题

--- src/doublePointer/slideWindow.java
+++ src/doublePointer/slideWindow.java
@@ -1,5 +1,7 @@
 package doublePointer;
 
+import java.nio.channels.NonWritableChannelException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -24,7 +26,6 @@ public class slideWindow {
 		//创建两个hashMap来分别来存储，所需char和窗口中已经存在char的数量
 		Map<Character, Integer> needMap=new HashMap<>();
 		Map<Character, Integer> windowMap=new HashMap<>();
-
 		
 		// 初始化 needMap
 		for (int i=0;i<t.length();++i) {needMap.put(t.charAt(i), 0);}
@@ -50,8 +51,7 @@ public class slideWindow {
 			if (needMap.containsKey(c)) {
 				windowMap.put(c, windowMap.get(c)+1);
 				if (needMap.get(c)==windowMap.get(c)) {
-					valid++;
-					
+					valid++;					
 				}
 			}
 			//判断左侧窗口是否要收缩
@@ -74,11 +74,9 @@ public class slideWindow {
 					windowMap.put(d, windowMap.get(d)-1);
 				}
 			}			
-		}
-		
+		}		
 		// 返回最小字符串
-		return subStrLen==Integer.MAX_VALUE?"":s.substring(startIndex, startIndex+subStrLen);
-		
+		return subStrLen==Integer.MAX_VALUE?"":s.substring(startIndex, startIndex+subStrLen);		
 	}
 	
 	/*
@@ -108,32 +106,123 @@ public class slideWindow {
 			//操作
 			if (needMap.containsKey(c)) {
 				windowMap.put(c, windowMap.get(c)+1);
-				if (needMap.containsKey(c)) {
+				if (needMap.get(c)==windowMap.get(c)) {
 					valid++;
-				}
-												
+				}												
 			}
 			//当窗口长度等于 valid char 个数 表示，窗口内只含needmap中的char
 			//判断是否左收缩时，尽量用每次while循环都会改变的变量  subStr 连续用定窗口滑动
 			while (right-left>=subStr.length()) {
-				if(valid==subStr.length()) return true;
+				if(valid==needMap.size()) return true;
 				char d=str.charAt(left);
 				left++;
 				//操作
 				if (needMap.containsKey(d)) {
-					if (needMap.containsKey(d)) {
+					if (needMap.get(d)==windowMap.get(d)) {
 						valid--;
 					}
 					windowMap.put(d, windowMap.get(d)-1);
-				}
-		
-			}
-			
-			
-		}
-		
+				}		
+			}						
+		}		
 		return false;
 	}
+	
+	
+	
+	
+	/*
+	 * 3 找到所有字母异位词
+	 * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+	 *字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。 
+	 */
+	public static ArrayList<Integer> findAllAnagramsString(String s,String p) {
+		//创建needMap和windowMap来记录子字符
+		Map<Character, Integer> needMap=new HashMap<>();
+		Map<Character, Integer> windowMap=new HashMap<>();
+		
+		//初始化needMap and windowMap
+		for (int i = 0; i < p.length(); i++) {needMap.put(p.charAt(i), 0);}
+		for (int i = 0; i < p.length(); i++) {needMap.put(p.charAt(i), needMap.get(p.charAt(i))+1);}
+		for (int i = 0; i < s.length(); i++) {windowMap.put(s.charAt(i), 0);}
+		
+		//初始化指针索引
+		int left=0;
+		int right=0;
+		int valid=0;
+		
+		ArrayList<Integer> resultList=new ArrayList<Integer>();
+		
+		//判断是否遍历晚字符串
+		while (right<s.length()) {
+			char c=s.charAt(right);
+			right++;			
+			//字符串内部操作
+			if (needMap.containsKey(c)) {
+				windowMap.put(c, windowMap.get(c)+1);
+				if (needMap.get(c)==windowMap.get(c)) {
+					valid++;
+				}				
+			}
+			
+		//判断是否 需要进行左收缩
+			while (right-left>=p.length()) {
+				if (valid==needMap.size()) {
+					resultList.add(left);			
+				}			
+				char d =s.charAt(left);
+				left++;
+				if (needMap.containsKey(d)) {
+					if (needMap.get(d)==windowMap.get(d)) {
+						valid--;
+					}					
+					windowMap.put(d, windowMap.get(d)-1);					
+				}
+			}			
+		}
+		return resultList;
+	}
+	
+	
+	
+	
+	/*
+	 * 4 最长无重复子串
+	 * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+	 */
+	public static int longestSubstrWithoutRepeating(String s) {
+		//创建needMap和windowMap存储字符串
+		Map<Character, Integer> windowMap=new HashMap<>();
+		
+		//初始化
+		for (int i = 0; i < s.length(); i++) {windowMap.put(s.charAt(i), 0);}
+		
+		//初始化索引
+		int left=0;
+		int right=0;
+		int valid=0;
+		
+		//window向右边扩张
+		while (right<s.length()) {
+			char c=s.charAt(right);
+			right++;			
+			windowMap.put(c, windowMap.get(c)+1);
+
+							
+			// 判断是不是需要收缩左窗口
+			while (windowMap.get(c)>1) {
+				char d=s.charAt(left);
+				left++;
+				//对窗口进行数据操作
+				windowMap.put(d, windowMap.get(d)-1);
+			}
+			valid=Math.max(valid, right-left);
+					
+		}
+		return valid;
+	}
+	
+	
 	
 	public static void main(String[] args) {
 //		System.out.println("请输入字符串s:");
@@ -147,10 +236,21 @@ public class slideWindow {
 //		String substr=minSubCoverage(s,t); 
 		
 		
-		String s="ab";
-		String t="eidbobaoo";
-		boolean substr=ifHasSubStr(s,t);
-		System.out.println("结果为："+substr);
+//		String s="abb";
+//		String t="eidbobbaoo";
+//		boolean substr=ifHasSubStr(s,t);
+//		System.out.println("结果为："+substr);
+		
+//		String s="cbaebabacd";
+//		String t="abc";
+//		System.out.println(findAllAnagramsString(s,t));
+		
+		
+		
+		String s="abcabcbb";
+
+		System.out.println(longestSubstrWithoutRepeating(s));		
+		
 	}
 	
 }
