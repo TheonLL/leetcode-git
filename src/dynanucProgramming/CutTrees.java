cc3d67ba34a2968d3760c01e3b2e1309b1040c27
theonl-217 <glruan@foxmail.com>
2021-07-25 21:30:07 +0800

    add

--- src/dynanucProgramming/CutTrees.java
+++ src/dynanucProgramming/CutTrees.java
@@ -10,7 +10,6 @@ public class CutTrees {
 		SolutionCutTrees solutionCutTrees=new SolutionCutTrees();
 		System.out.println(solutionCutTrees.cutCost(9, cuts));
 		}
-
 }
 
 
@@ -22,45 +21,51 @@ class SolutionCutTrees{
 		nowTreeLength.add(0);
 		nowTreeLength.add(TreeLength);
 		backTrack(nowTreeLength, cuts,0);
-		System.out.println("==================");
+//		System.out.println("==================");
 		Collections.sort(costList);
-		System.out.println(costList.toString());
-		
-		return costList.get(costList.size()-1);				
+//		System.out.println(costList.toString());
+//		System.out.println(costList.size());
+		return costList.get(0);				
 	}
 	private void backTrack(List<Integer> nowTreeLength,int[] nowCuts,int index) {
 			//回溯结束条件
-			for (int i = index; i <nowCuts.length; ++i) {
-
-					System.out.println("indes");
-					System.out.println(i);
-					System.out.println(nowCuts[i]) ;
-					//选择切入点
-					int nowLength=findLen(nowTreeLength, nowCuts[i]);
-					
-					allCost+=nowLength;
-					System.out.println(allCost);
-					nowTreeLength.add(nowCuts[i]);
-					if (i+1==nowCuts.length) {
-						System.out.println("-----------------");
-						System.out.println(allCost);
-						costList.add(allCost);
-						return;
-					}
-					backTrack(nowTreeLength, nowCuts, i+1);
-					nowTreeLength.remove(nowTreeLength.size()-1);
-					allCost-=nowLength;
+			for (int i = 0; i <nowCuts.length; ++i) {
+				if(nowTreeLength.contains(nowCuts[i])) {
+					continue;
 				}
-
+//					System.out.println("indes");
+//
+//					System.out.println(nowCuts[i]) ;
+					//选择切入点
+					int nowLength=findLen(nowTreeLength, nowCuts[i]);					
+					allCost+=nowLength;
+//					System.out.println(allCost);
+					nowTreeLength.add(nowCuts[i]);
+					if (nowTreeLength.size()==nowCuts.length+2) {
+//						System.out.println("-----------------");
+//						System.out.println(allCost);
 						
-	}
-	
+						costList.add(allCost);
+//						System.out.println(costList.toString());
+//						return;
+					}
+						backTrack(nowTreeLength, nowCuts, i+1);
+//						System.out.println("----++++++++-------");
+//						System.out.println(nowCuts[i]);
+						nowTreeLength.remove(nowTreeLength.indexOf(nowCuts[i]));
+//						System.out.println(nowTreeLength.toString());
+//						System.out.println(allCost);
+						allCost-=nowLength;	
+//						System.out.println(allCost);
+					
+
+				}					
+	}	
 	private int findLen(List<Integer> nowTreeLength,int cut) {
 		int left=0;
 		int right=0;
 		Collections.sort(nowTreeLength);
-		System.out.println(nowTreeLength.toString());
-		
+//		System.out.println(nowTreeLength.toString());		
 		for (Integer integer : nowTreeLength) {
 			if (integer<cut) {
 				left=integer;
@@ -69,8 +74,8 @@ class SolutionCutTrees{
 				break;
 			}
 		}
-		System.out.println("cheng");
-		System.out.println(right-left);
+//		System.out.println("cheng");
+//		System.out.println(right-left);
 		return right-left;
 	}
 }
