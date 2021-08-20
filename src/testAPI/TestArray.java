a4e4399b7892d8a6307ce6382fa52a8408a989b5
glence <GLruan@foxmail.com>
2021-08-20 19:32:51 +0800

    add

---
+++ src/testAPI/TestArray.java
@@ -0,0 +1,25 @@
+package testAPI;
+
+import java.util.Arrays;
+
+/**
+ * @author: 11983
+ * @date: 21/08/20 16:44
+ * @description:
+ */
+
+public class TestArray {
+    private int[] array1 = new int[]{1,3,4,5};
+    public static void main(String[] args) {
+
+        int[] array = new int[]{1,3,4,5};
+        int[] array1 = new int[3];
+        int array2[]=new int[]{1,3,4,5};
+        System.out.println(Arrays.toString(array2));
+
+
+    }
+    public static void test() {
+        
+}
+}
